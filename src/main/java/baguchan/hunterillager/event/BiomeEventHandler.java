b844ae133b7608a38ab7f4c3b94b6efbe219334f
shutux <shutux500@gmail.com>
2020-09-26 06:38:14 +0900

    no longer spawn hunterhouse on end and nether biome

--- src/main/java/baguchan/hunterillager/event/BiomeEventHandler.java
+++ src/main/java/baguchan/hunterillager/event/BiomeEventHandler.java
@@ -3,6 +3,7 @@ package baguchan.hunterillager.event;
 import baguchan.hunterillager.HunterConfig;
 import baguchan.hunterillager.HunterIllagerCore;
 import baguchan.hunterillager.structure.StructureRegister;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,7 +14,13 @@ public class BiomeEventHandler {
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void addSpawn(BiomeLoadingEvent event) {
         if (HunterConfig.generateHunterHouse) {
-            event.getGeneration().withStructure(StructureRegister.HUNTER_HOUSE);
+            if (event.getName().getNamespace().toString().contains("minecraft") || event.getName().getNamespace().toString().contains("biomesoplenty")) {
+                if (event.getCategory() != Biome.Category.NETHER && event.getCategory() != Biome.Category.THEEND) {
+                    event.getGeneration().withStructure(StructureRegister.HUNTER_HOUSE);
+                }
+
+            }
+
         }
     }
 }
